359834c810b8234eccbf247bdfcca8a8baff3f67
BlindEka <ekazero99@gmail.com>
2025-09-09 15:00:54 +0700

    telah belajar syntax new Function dan penjadwalan pemanggilan function

--- random.js
+++ random.js
@@ -26,42 +26,48 @@ function enumerate(iterable, start=0) {
   return Array.from(iterable).map((item, index) => [index + start, item]);
 };
 
-const Random = {
-  random() {
-    return Math.random();
-  },
-  randint(min, max) {
-        return Math.round(min - 0.5 + this.random() * (max - min + 1));
-  },
-  choice(seq) {
-    return seq.length > 0 ? seq[this.randint(0, seq.length - 1)] : null;
-  },
-  choices(seq, k=1) {
-    return ! seq.length || k <= 0 ? null : Array.from(range(k)).map((v,i,arr) => this.choice(seq));
-  },
-  randrange(...[start, stop, step]) {
-    return arguments.length > 1 ? this.choice(Array.from(range(start, stop, step || 1))) : this.choice(Array.from(range(start)));
-  },
-  uniform(min=0, max=1) {
-    return min > max ? null : min + this.random() * (max - min);
-  },
-  randbool() {
-    return Boolean(Math.round(this.random()));
-  },
-  sample(seq, k) {
-    if (k > seq.length) return null;
-    seq = seq.slice();
-    let sample = [];
-    for (i of range(k)) {
-      sample.push(...seq.splice(this.randint(0, seq.length - 1), 1));
-    };
-    return sample;
-  },
-  shuffle(seq) {
-    seq.forEach((value, index, array) => {
-      let randomNumber = this.randint(0, seq.length - 1);
-      [seq[index], seq[randomNumber]] = [seq[randomNumber], seq[index]];
-    });
-    return seq;
-  },
+function random() {
+  return Math.random();
+}
+
+random.random = random;
+
+random.randint = function randint(min, max) {
+      return Math.round(min - 0.5 + this.random() * (max - min + 1));
+};
+
+random.choice = function choice(seq) {
+  return seq.length > 0 ? seq[this.randint(0, seq.length - 1)] : null;
+};
+
+random.choices = function choices(seq, k=1) {
+  return ! seq.length || k <= 0 ? null : Array.from(range(k)).map((v,i,arr) => this.choice(seq));
+};
+random.randrange = function randrange(...[start, stop, step]) {
+  return arguments.length > 1 ? this.choice(Array.from(range(start, stop, step || 1))) : this.choice(Array.from(range(start)));
+};
+random.uniform = function uniform(min=0, max=1) {
+  return min > max ? null : min + this.random() * (max - min);
+};
+
+random.randbool = function randbool() {
+  return Boolean(Math.round(this.random()));
+};
+
+random.sample = function sample(seq, k) {
+  if (k > seq.length) return null;
+  seq = seq.slice();
+  let sample = [];
+  for (i of range(k)) {
+    sample.push(...seq.splice(this.randint(0, seq.length - 1), 1));
+  };
+  return sample;
+};
+
+random.shuffle = function shuffle(seq) {
+  seq.forEach((value, index, array) => {
+    let randomNumber = this.randint(0, seq.length - 1);
+    [seq[index], seq[randomNumber]] = [seq[randomNumber], seq[index]];
+  });
+  return seq;
 };
